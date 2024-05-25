948f91818517e2913e658a24584e2bd213a8726a
uaquax <uaquax@tutanota.com>
2024-05-25 20:40:46 +0800

    Init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,36 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import s from "./page.module.sass";
+import { LogOut, HomeIcon, User2, Settings, History, Search } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={s.container}>
+      <div className={s.sidebar}>
+        <div className={s.tabs}>
+          <button className={`${s.button} ${s.active}`}>
+            <HomeIcon className={s.icon} color="#ffffff" />
+          </button>
+          <button className={s.button}>
+            <User2 className={s.icon} color="#ffffff" />
+          </button>
+          <button className={s.button}>
+            <Settings className={s.icon} color="#ffffff" />
+          </button>
+          <button className={s.button}>
+            <History className={s.icon} color="#ffffff" />
+          </button>
+          <button className={s.button}>
+            <Search className={s.icon} color="#ffffff" />
+          </button>
+        </div>
+        <div className={s.logout}>
+          <button className={s.button}>
+            <LogOut className={s.icon} color="#ffffff" />
+          </button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={s.content}>
+        <h1>Moovy</h1>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
